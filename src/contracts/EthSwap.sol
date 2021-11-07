b9604a8c0b02e9119c7c27f9315a3a79a97e2db9
himandrishram27 <himandrisharma27@gmail.com>
2021-11-07 14:52:23 +0530

    truffle transfer successful

---
+++ src/contracts/EthSwap.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: GPL-3.0-or-laterdd
+pragma solidity >=0.4.21;
+
+contract EthSwap {
+    string public name = "EthSwap Instant Exchange";
+}
--- src/contracts/Migrations.sol
+++ src/contracts/Migrations.sol
@@ -1,4 +1,5 @@
-pragma solidity >=0.4.21 <0.6.0;
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity >=0.4.21;
 
 contract Migrations {
   address public owner;
